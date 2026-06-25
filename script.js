146967ff0d2cdc00ba523b6e38501246770866b0
◡◡ 。basil.  .ᐟ.ᐟ ✦、 <giyuusnyancats@gmail.com>
2026-06-25 15:10:28 -0500

    Add dialogue functionality to mewo click event

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-const mewo = document.getElementById("mewo");
+const mewo =
+document.getElementById("mewo");
+
+const dialogue =
+document.getElementById("dialogue");
+
+const dialogueName =
+document.getElementById("dialogue-name");
+
+const dialogueText =
+document.getElementById("dialogue-text");
+
+let action = null;
+
+function showDialogue(
+    name,
+    text,
+    callback = null
+){
+
+    dialogueName.textContent = name;
+
+    dialogueText.textContent = text;
+
+    action = callback;
+
+    dialogue.classList.remove("hidden");
+}
+
+dialogue.addEventListener("click", () => {
+
+    if(action){
+
+        action();
+
+        return;
+    }
+
+    dialogue.classList.add("hidden");
+});
 
 mewo.addEventListener("click", () => {
-    window.location.href = "okay.mp4";
+
+    showDialogue(
+        "MEWO",
+        "MEWO wants to show you something.",
+        () => {
+
+            window.location.href = "okay.mp4";
+
+        }
+    );
+
 });
